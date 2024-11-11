6892a25adef50642ccf188007d0bde78123f0a60
yura.cheglakov <yura.cheglakov@gmail.com>
2024-11-11 22:44:19 +0300

    add demoqa

--- tests/demoqa/test_registration.py
+++ tests/demoqa/test_registration.py
@@ -1,5 +1,25 @@
 from selene import browser
-
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def test_add_content():
+    options = Options()
+    capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+
+    options.capabilities.update(capabilities)
+    options.page_load_strategy = "eager"
+    options.add_argument("window-size=1920,1080")
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+    browser.config.driver = driver
     browser.open('https://demoqa.com/automation-practice-form')
+    browser.quit()
