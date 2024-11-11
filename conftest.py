4f536536f4650acdd0551db965b3d9eb4683195d
yura.cheglakov <yura.cheglakov@gmail.com>
2024-11-11 21:30:23 +0300

    add demoqa

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from selene import browser, Browser, Config
-
+from selenium import webdriver
+from selenium.webdriver.chrome.options import import Options
 
 
 @pytest.fixture(scope='function')
@@ -16,7 +17,20 @@ def open_browser(request):
     # options.add_argument('--disable-dev-shm-usage')
     # options.add_argument('--disable-setuid-sandbox')
     # browser.config.driver_options = options
+    from selenium import webdriver
 
-    browser.open('https://demoqa.com/automation-practice-form')
+    capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        desired_capabilities=capabilities)
+    # browser.open('https://demoqa.com/automation-practice-form')
     yield
     browser.quit()
