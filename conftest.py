18b301df7843ea94757cf8b4b00270961dae117b
yura.cheglakov <yura.cheglakov@gmail.com>
2024-11-11 22:18:12 +0300

    add demoqa

--- conftest.py
+++ conftest.py
@@ -4,29 +4,27 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-@pytest.fixture(scope='function', autouse=True)
+@pytest.fixture(scope="function", autouse=True)
 def setup_browser(request):
-    @pytest.fixture(scope="function", autouse=True)
-    def setup_browser(request):
-        browser.config.base_url = "https://demoqa.com/automation-practice-form/"
-        options = Options()
-        capabilities = {
-            "browserName": "chrome",
-            "browserVersion": "100.0",
-            "selenoid:options": {
-                "enableVNC": True,
-                "enableVideo": True
-            }
+    browser.config.base_url = "https://demoqa.com/automation-practice-form/"
+    options = Options()
+    capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
         }
+    }
 
-        options.capabilities.update(capabilities)
-        options.page_load_strategy = "eager"
-        options.add_argument("window-size=1920,1080")
-        driver = webdriver.Remote(
-            command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-            options=options)
+    options.capabilities.update(capabilities)
+    options.page_load_strategy = "eager"
+    options.add_argument("window-size=1920,1080")
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
 
-        browser.config.driver = driver
+    browser.config.driver = driver
 
-        yield browser
+    yield browser
     browser.quit()
